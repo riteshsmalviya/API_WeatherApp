3b2f794766d397cc0aaad883f5ded9832b5d7bbf
Ritesh Malviya <115831260+riteshsmalviya@users.noreply.github.com>
2024-11-11 18:14:10 +0530

    Script.js

--- script.js
+++ script.js
@@ -2,28 +2,40 @@ const options = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Key': '0e44c59556msh84c241951b4990dp1fa891jsn0b73257090d6',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
     }
 };
-const getWeather = (city) =>{
-    cityName.innerHTML = city
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-    .then(response => response.json())
-    .then(response =>{
-        console.log(response)
-        temp.innerHTML = response.temp
-        feels_like.innerHTML = response.feels_like
-        min_temp.innerHTML = response.min_temp-2
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-        wind_degrees.innerHTML = response.wind_degrees
-})
-    .catch(err => console.error(err));
+
+const getWeather = (city) => {
+    cityName.innerHTML = city;
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            // Populate data based on the updated JSON structure
+            temp.innerHTML = response.current.temp_c;
+            feels_like.innerHTML = response.current.feelslike_c;
+            wind_speed.innerHTML = response.current.wind_kph;
+            wind_degrees.innerHTML = response.current.wind_degree;
+            wind_direction.innerHTML = response.current.wind_dir;
+            humidity.innerHTML = response.current.humidity;
+            pressure.innerHTML = response.current.pressure_mb;
+            
+        })
+    fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            max_temp.innerHTML = response.forecast.forecastday[0].day.maxtemp_c;
+            min_temp.innerHTML = response.forecast.forecastday[0].day.mintemp_c;
+            
+        })
+        .catch(err => console.error(err));
 }
 
-submit.addEventListener("click", (e)=>{
-    e.preventDefault()
-    getWeather(city.value)
-})
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value);
+});
 
-getWeather("Delhi")
+getWeather("Indore");
